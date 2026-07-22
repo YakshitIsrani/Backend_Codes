c3229f060c3af228300db5d0c0eb801e42ccaf84
YakshitIsrani <yakshitisrani@gmail.com>
2026-07-22 14:47:40 +0530

    Second commit

--- Day02/server.js
+++ Day02/server.js
@@ -3,7 +3,15 @@
 const http=require("http");
 
 const server=http.createServer((req,res)=>{
-    res.end("Samjha bhiduuu");
+    //Handling Routing in node js server
+    if(req.url==="/")
+        res.end("Home Page");
+    else if (req.url==="/about")
+        res.end("About Page")
+    else if (req.url==="/payment")
+        res.end("Payment Page");
+    else res.end("Page not found")
+    
 })
 
 server.listen(4000,()=>{
